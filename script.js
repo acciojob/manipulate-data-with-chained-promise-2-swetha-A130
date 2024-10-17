0bcaca66248f071041d11311505f2ee56400959b
swetha-A130 <swethachintalapudi6@gmail.com>
2024-10-17 15:42:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+function getNumbers() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([1, 2, 3, 4]);  // or any array of numbers
+    }, 3000);  // 3 seconds
+  });
+}
+getNumbers().then(numbers => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      const evenNumbers = numbers.filter(number => number % 2 === 0);
+      resolve(evenNumbers);
+    }, 1000);
+  });
+})
+then(evenNumbers => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      const doubledNumbers = evenNumbers.map(number => number * 2);
+      resolve(doubledNumbers);
+    }, 2000);
+  });
+})
+.then(evenNumbers => {
+  document.getElementById('output').innerText = evenNumbers.toString();
+});
+.then(doubledNumbers => {
+  document.getElementById('output').innerText = doubledNumbers.toString();
+});
