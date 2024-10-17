e03621b7366678eb9af3c9bdbf5acb3df02770ee
swetha-A130 <swethachintalapudi6@gmail.com>
2024-10-17 15:48:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,21 +5,21 @@ function getNumbers() {
     }, 3000);  // 3 seconds
   });
 }
-
 getNumbers().then(numbers => {
   return new Promise(resolve => {
     setTimeout(() => {
       const evenNumbers = numbers.filter(number => number % 2 === 0);
+      document.getElementById('output').innerText = evenNumbers.toString();
       resolve(evenNumbers);
     }, 1000);
   });
-}).then(evenNumbers => {
+})
+.then(evenNumbers => {
   return new Promise(resolve => {
     setTimeout(() => {
       const doubledNumbers = evenNumbers.map(number => number * 2);
+      document.getElementById('output').innerText = doubledNumbers.toString();
       resolve(doubledNumbers);
     }, 2000);
   });
-}).then(doubledNumbers => {
-  document.getElementById('output').innerText = doubledNumbers.toString();
 });
